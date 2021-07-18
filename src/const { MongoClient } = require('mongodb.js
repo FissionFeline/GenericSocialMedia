dcc7331292a322d46dfac505415dea85a2366fd6
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2021-07-19 01:47:07 +0200

    Deleted test files

--- src/const { MongoClient } = require('mongodb.js
+++
@@ -1,9 +0,0 @@
-
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://8uzdw8dw8d88773:<password>@usrdata.z1i9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient("mongodb+srv://8uzdw8dw8d88773:<password>@usrdata.z1i9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
